eba12a23b270c4c869c0538b900936f3116a9efc
fiza0786 <92218448+fiza0786@users.noreply.github.com>
2021-10-09 19:53:11 +0530

    Fiza_addition.c
    Hello  I am beginner in Coding I made a addition programm

--- Arithmetic/C/maleeshasilva_simpleArithmeticOperation.c
+++ Arithmetic/C/maleeshasilva_simpleArithmeticOperation.c
@@ -1,23 +1,10 @@
 #include<stdio.h>
+void main()
+{   // It is  my addition programm 
+    float number1, number2, result;
+ printf("Enter the number to add \n");
+    scanf("%f %f",&number1,&number2);
+ result = number1 + number2;
+ printf("%f + %f = %f",result); // output result 
 
-int main()
-{
-    
-    int x, y, add, subtract, multiply;
-    float divide;
-
-    printf("Enter two integers: \n");
-    scanf("%d%d", &x, &y);
-
-    add = x+y;
-    subtract = x-y;
-    multiply = x*y;
-    divide = x/y;
-
-    printf("\nAddition of the numbers = %d\n", add);
-    printf("Subtraction of 2nd number from 1st = %d\n", subtract);
-    printf("Multiplication of the numbers = %d\n", multiply);
-    printf("Dividing 1st number from 2nd = %f\n", divide);
-    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
-    return 0;
 }
